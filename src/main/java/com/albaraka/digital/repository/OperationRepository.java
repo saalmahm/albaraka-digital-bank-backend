0afc8c8364cb2a8f5af8c1b6a4d370131a3649b1
salma hamdi <saalmahm51@gmail.com>
2025-12-19 15:26:58 +0100

    ABB-31 Ajout d'une méthode paginée pour lister les opérations par compte source ou destination

--- src/main/java/com/albaraka/digital/repository/OperationRepository.java
+++ src/main/java/com/albaraka/digital/repository/OperationRepository.java
@@ -1,7 +1,16 @@
 package com.albaraka.digital.repository;
 
+import com.albaraka.digital.model.entity.Account;
 import com.albaraka.digital.model.entity.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
+
+    Page<Operation> findByAccountSourceOrAccountDestination(
+            Account accountSource,
+            Account accountDestination,
+            Pageable pageable
+    );
 }
