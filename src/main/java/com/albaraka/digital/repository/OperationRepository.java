5d6dd64f3d47a8dd749b40164aea8dd127cbcebf
salma hamdi <saalmahm51@gmail.com>
2025-12-19 23:48:09 +0100

    feat(operation): ajouter une méthode repository pour lister les opérations PENDING

--- src/main/java/com/albaraka/digital/repository/OperationRepository.java
+++ src/main/java/com/albaraka/digital/repository/OperationRepository.java
@@ -2,6 +2,7 @@ package com.albaraka.digital.repository;
 
 import com.albaraka.digital.model.entity.Account;
 import com.albaraka.digital.model.entity.Operation;
+import com.albaraka.digital.model.enums.OperationStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,6 @@ public interface OperationRepository extends JpaRepository<Operation, Long> {
             Account accountDestination,
             Pageable pageable
     );
+
+    Page<Operation> findByStatus(OperationStatus status, Pageable pageable);
 }
